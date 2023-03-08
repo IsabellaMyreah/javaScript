7fa2b6250ae01bc670ea1d45db3b12dd5f0dde0a
Isabella Cavalcante <108815118+IsabellaMyreah@users.noreply.github.com>
2023-03-08 12:33:21 -0300

    Add files via upload

---
+++ atividade.modal/script.js
@@ -0,0 +1,9 @@
+function abrirModal(){
+    document.getElementById('modal').style.display = 'flex'
+}
+
+function fecharModal(){
+    document.getElementById('modal').style.display = 'none'
+}
+
+console.log(fecharModal())
